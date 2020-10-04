2e69ac946acdd3f5bf80896f66ee4d42a4b5d4ba
SteadyWeb <steady.web.0@gmail.com>
2020-10-04 08:03:33 +0600

    uploaded initial project structure

---
+++ Lex-Diary-Admin-Panel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lex_Diary_Admin_Panel.MvcApplication" Language="C#" %>
---
+++ Lex-Diary-Admin-Panel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lex_Diary_Admin_Panel.Startup))]
+namespace Lex_Diary_Admin_Panel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
